c703ae4753f702bb3a4607b032af2744f9299054
Vanesa <vanesatov@gmail.com>
2023-12-13 11:14:20 +0100

    creación de index

---
+++ entorno.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Examen ED</title>
+    <link rel="stylesheet" href="css/estilo.css">
+    
+</head>
+<body>
+    <div id="principal">
+        <h1>VICTOR MANUEL NAVARRO CAMINO</h1>
+        <div>
+            <img src="img/nieve.jpg" alt="Foto de la nieve">
+        </div>
+        <br>
+
+        <?php
+            echo "<h2>Examen Entorno de Desarrollo. Suerte!</h2>";
+        ?>
+    </div>
+</body>
+</html>
